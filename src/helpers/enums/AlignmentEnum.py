2f5c2572aac0bc56b6c2e53ffb0a1985b597ffca
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-03-20 18:39:36 +0300

    [FIX] "Enum" folder name change to "enum"

---
+++ src/helpers/enums/AlignmentEnum.py
@@ -0,0 +1,12 @@
+"""
+Enum class for alignment style of element
+"""
+
+from enum import Enum
+
+
+class AlignmentEnum(Enum):
+    LEFT = 'left'
+    RIGHT = 'right'
+    CENTER = 'center'
+    JUSTIFY = 'juctify'
