4532663fbd45f1c01c5ab6a6de2c37e42115d010
slavamarcin <102062218+slavamarcin@users.noreply.github.com>
2023-05-06 02:06:56 +0300

    [Feat] Added files for further writing tests for PDF classes

--- examples/pdf_example/PDFParserExample.py
+++ examples/pdf_example/PDFParserExample.py
@@ -1,7 +1,5 @@
-from src.classes.UnifiedDocumentView import UnifiedDocumentView
 from src.pdf.PDFParser import PDFParser
 from os import walk
-from requests.exceptions import ConnectionError
 
 for dir_path, dir_names, file_names in walk('.\\documents'):
     for filename in file_names:
@@ -18,18 +16,11 @@ for dir_path, dir_names, file_names in walk('.\\documents'):
         Using the get_elements method, we get a file of the UnifiedDocumentView type, 
         which contains data about the entire text document and its structural elements
         '''
-        document = pdf_parser.get_elements(lines, spaces, tables, list_of_picture)
+        document = pdf_parser.get_all_elements(lines, spaces, tables, list_of_picture)
         # To write information about structural elements, use the write_CSV method, specifying the save path
-        document.write_CSV(dir_path + '\\' + filename + '.csv')
+        # document.write_CSV(dir_path + '\\' + filename + '.csv')
         '''
         To create a JSON string from data about structural elements, which will later be sent to the classifier,
         use the create_json_to_clasifier method, which takes a list of required fields as parameters
         '''
-        json = document.create_json_to_clasifier()
-        '''
-        To send data to the classifier, use the request_to_clasify method, specifying the string in the json parameters
-        '''
-        try:
-            UnifiedDocumentView.request_to_clasify(json)
-        except ConnectionError:
-            print("No connection with clasifier")
+        json = document.create_json()
---
+++ tests/pdf/TestPDFParser.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestPDFParser(unittest.TestCase):
+
+    def test_parse_content(self):
+        pass
+
+    def test_parse_pictures(self):
+        pass
+
